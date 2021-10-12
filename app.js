512038600294455a3d3e2608ea7f32b43ba627e7
rishin <mrishinv@gmail.com>
2021-10-12 22:56:51 +0530

    emojis added

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("emoji-btn");
+const emojis = [
+    "😁", "😂", "🙂", "🙃", "😉", "🥰", "😇", "😍", 
+    "🤩", "😘", "😗", "😚", "😋", "🤑", "🤗", "🤨", 
+    "🤭", "🤫", "🤐", "😐", "😑", "😶‍🌫️", "🙄",
+];
+
+btn.addEventListener("mouseover", () => {
+    btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+
+});
